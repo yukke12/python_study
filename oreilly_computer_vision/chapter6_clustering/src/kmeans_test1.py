eb038a1be6ed39217a0a287d4399b56a1bde646f
shiraiyusuke <memetta2002@yahoo.co.jp>
2016-04-09 14:08:43 +0900

    test

--- oreilly_computer_vision/chapter6_clustering/src/kmeans_test1.py
+++ oreilly_computer_vision/chapter6_clustering/src/kmeans_test1.py
@@ -4,13 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def drow_result(features, code):
+def drow_result(features, code, centroids):
+    """
+
+    :param features:
+    :param code:
+    :param centroids:
+    :return:
+    """
+
     plt.figure()
     ndx = np.where(code == 0)[0]
-    plt.plot(features[ndx, 0], features[ndx, 1])
-
+    print ndx
+    plt.plot(features[ndx, 0], features[ndx, 1],'*')
+    ndx = np.where(code == 1)[0]
+    plt.plot(features[ndx, 0], features[ndx, 1],'r')
+    plt.plot()
 
 def kmeans_clustering():
+    """
+    abstruct:
+        標準正規分布で100行2列のベクトルを2つ発生し、それをvstackで結合した特徴量にk=2のkmeansを実行。
+    :return:
+        features:特徴量
+        code:属するクラスタ番号
+        centroids:重心座標
+    """
     # random.randn(100, 2): generate 100 number 2 dimension at Gaussian distribution random float number
     class1 = 1.5 * np.random.randn(100, 2)
     # 標準正規分布で発生させた100行2列に、5,5を足す。
@@ -26,10 +45,10 @@ def kmeans_clustering():
     code, distance = vq(features, centroids)
     # クラスタ1に割り当てられた数をカウント。
     # print np.count_nonzero((code > 0.5)*1)
-    return features, code
+    return features, code, centroids
 
 if __name__ == '__main__':
     # 標準正規分布で発生させた100*2のベクトルをkmeans(k=2)でクラスタリングし、その実行結果を取得。
     # features:特徴量、code:クラスタリング結果(0 or 1)
     features, code = kmeans_clustering()
-    drow_result(features, code)
+    drow_result(features, code, centroids)
