794a73a9a225cb705782dfa2eb75b0f36531d919
shiraiyusuke <memetta2002@yahoo.co.jp>
2016-04-09 12:14:36 +0900

    [new] oreillly_computer_vision

---
+++ oreilly_computer_vision/chapter6_clustering/kmeans_test1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from scipy.cluster.vq import *
+import numpy as np
+
+def kmeans_clustering():
+    # random.randn(100, 2): generate 100 number 2 dimension at Gaussian distribution random float number
+    class1 = 1.5 * np.random.randn(5, 2)
+
+if __name__ == '__main__':
+    kmeans_clustering()
