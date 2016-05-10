3c882d819c05a6a8eb01138b97b662187d464477
shiraiyusuke <memetta2002@yahoo.co.jp>
2016-05-11 00:34:13 +0900

    [add] flaski display

---
+++ flask_test/src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def hello():
+    # title='hogehoge'で指定した物は、index.html内で{{}}で囲って展開できる。
+    return render_template("index.html", title="Flaski")
+
+if __name__ == "__main__":
+    app.run()
